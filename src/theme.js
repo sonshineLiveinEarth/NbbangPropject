6bc877caa903b1c1a6941cecffd6302b00d4406d
kimjinhee <kjh11071@gmail.com>
2022-06-11 14:06:30 +0900

    커밋 정리

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+const device = {
+    tablet: `@media screen and (min-width: 768px)`,
+    desktop: `@media screen and (min-width: 1024px)`,
+  };
+  
+  // 주로 사용하는 색상을 theme으로 지정
+  const colors = {
+    mainColor: "#2AC1BC", // mint
+    yellowGreen: "#C8DF52",
+    mint: "#DBE8D8",
+    blue: "#0984e3",
+    white: "#FFFFFF",
+    black: "000000",
+  };
+  
+  const fontSizes = {
+    xs: "12px",
+    sm: "14px",
+    md: "16px",
+    lg: "20px",
+    xl: "24px",
+  };
+  
+  const theme = {
+    device,
+    colors,
+    fontSizes,
+  };
+  
+  export default theme;
+  
